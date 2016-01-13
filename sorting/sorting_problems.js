82ee8eb6970106b7884c9cacffd4f7798d00c9d1
Carl Bolstad <cbolstad@gmail.com>
2016-01-13 14:47:55 -0800

    add problems 11.2 and 11.6

--- sorting/sorting_problems.js
+++ sorting/sorting_problems.js
@@ -9,3 +9,58 @@ exports.mergeTwoSortedArrays = function (A, B) {
 
 //mergeTwoSortedArrays(A, B);
 //console.log(A);
+
+//11.2 Write a method to sort an array of strings so that all the anagrams are next each other
+
+//11.2 Given a sorted array of n integers that has been rotated an unknown number of times, write code to find an element in the array.
+//You smay assume that the array was orginally sorted in increasing order.
+// ex: input: find 5 in (15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14)
+//     output: 8 (index of 5 in the array)
+exports.indexOfIntegerInArray = function indexOfIntegerInArray(integer, array, left, right) {
+  debugger;
+  var searchIndex = Math.floor((left + right) / 2);
+
+  if(array[searchIndex] == integer) {
+    return searchIndex;
+  } else if(array[searchIndex] > integer) {
+    if(array[0] < integer) {
+      //found in left half: recurse or iterate
+      return indexOfIntegerInArray(integer, array, left, searchIndex);
+    } else {
+      //must be in right half:
+      return indexOfIntegerInArray(integer, array, searchIndex, right);
+    }
+  } else {
+    //must be in right half:
+    return indexOfIntegerInArray(integer, array, searchIndex, right);
+  }
+
+}
+
+//11.6 Given an MxN matrix in which each row and each column is sorted in ascending order, write a method to find an element.
+// 1  2  3
+// 4  5  6
+// 7  8  9
+//
+// 1  5  8
+// 2  7  9
+// 3  10 11
+
+// a. scan last column to determine upper row bounds (if lastelement > element stop)
+// b. scan last column to determine lower row bounds, starting at upper bound (if lastelement < element stop)
+// c. binary search on isolated row
+
+exports.findElementInSortedMatrix = function findElementInSortedMatrix(element, matrix) {
+  debugger;
+  var row = 0;
+  var col = matrix[0].length - 1;
+  while (row < matrix.length && col >= 0) {
+    if(matrix[row][col] == element) {
+      return true;
+    } else if (matrix[row][col] >  element) {
+      col --;
+    } else {
+      row ++;
+    }
+  }
+}
--- test/test_sorting.js
+++ test/test_sorting.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var mergeSort = require('../sorting/mergesort.js').mergeSort;
 var quickSort = require('../sorting/quicksort.js').quickSort;
 var mergeTwoSortedArrays = require('../sorting/sorting_problems.js').mergeTwoSortedArrays;
+var indexOfIntegerInArray = require('../sorting/sorting_problems').indexOfIntegerInArray;
+var findElementInSortedMatrix = require('../sorting/sorting_problems').findElementInSortedMatrix;
 
 describe('Sorting Problems', function() {
   describe('#mergesort', function () {
@@ -34,6 +36,23 @@ describe('Sorting Problems', function() {
       var B = [8, 9, 10, 11];
       mergeTwoSortedArrays(A, B);
       assert.equal(8, A[6]);
+
+    });
+  });
+
+  describe('#problem 11.2', function () {
+    it('should find integer in a rotated array', function () {
+      var A = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14];
+      var index = indexOfIntegerInArray(5, A, 0, A.length-1);
+      assert.equal(8, index);
+
+    });
+  });
+
+describe('#problem 11.6', function () {
+    it('should find element in a matrix', function () {
+      var matrix = [[15, 20, 40, 85], [20, 35, 80, 95], [30, 55, 95, 105], [40, 80, 100, 120]];
+      assert(findElementInSortedMatrix(55, matrix));
 
     });
   });
